edf1755aee274a552cbac4ca54212c9c956e3b8f
skabeo <spencerokyere95@gmail.com>
2023-05-23 14:53:30 +0100

    Dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Homepage from './components/Homepage';
 import IncomeStatement from './components/IncomeStatement';
 import BalanceSheet from './components/BalanceSheet';
 import Cashflow from './components/Cashflow';
 import Equity from './components/Equity';
+import { fetchIncomeData, fetchBalanceData, fetchCashData } from './redux/statements/statementsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchIncomeData());
+    dispatch(fetchBalanceData());
+    dispatch(fetchCashData());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/income-statement" element={<IncomeStatement />} />
