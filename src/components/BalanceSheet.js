679baf58fedccbbac12e527fa72f9263a765245b
skabeo <spencerokyere95@gmail.com>
2023-05-23 14:54:08 +0100

    Add balance sheet details page

--- src/components/BalanceSheet.js
+++ src/components/BalanceSheet.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Back from './Back';
+import styles from '../styles/Details.module.scss';
+import Filter from './Filter';
 
-const BalanceSheet = () => (
-  <div>BalanceSheet</div>
-);
+const BalanceSheet = () => {
+  const [filterValue, setFilterValue] = useState('');
+
+  const handleFilterChange = (value) => {
+    setFilterValue(value);
+  };
+
+  const data = useSelector((state) => state.data.balanceSheet);
+
+  const keysToFilter = ['cashAndCashEquivalents', 'shortTermInvestments', 'cashAndShortTermInvestments', 'netReceivables', 'inventory', 'totalCurrentAssets', 'propertyPlantEquipmentNet', 'longTermInvestments', 'totalAssets', 'accountPayable', 'shortTermDebt', 'deferredRevenue', 'otherCurrentLiabilities', 'totalCurrentLiabilities', 'totalLiabilities'];
+
+  const filteredData = Object.entries(data).filter(([key]) => keysToFilter.includes(key)
+    && key.toLowerCase().includes(filterValue.toLowerCase()));
+
+  return (
+    <div>
+      <Back />
+      <div className={styles.description}>
+        <p>Apple Inc</p>
+        <p>Balance Sheet</p>
+        <p>Year Ended December 31</p>
+      </div>
+      <div className={styles.operations}>
+        <div className={styles.searchField}>
+          <Filter
+            data={data}
+            filterValue={filterValue}
+            onFilterChange={handleFilterChange}
+          />
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>$</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.length === 0 ? (
+              <tr>
+                <td colSpan="2">No search results</td>
+              </tr>
+            ) : (
+              filteredData.map(([key, value]) => (
+                <tr key={key} className={styles.myTable}>
+                  <td>{key}</td>
+                  <td>{value.toLocaleString()}</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default BalanceSheet;
